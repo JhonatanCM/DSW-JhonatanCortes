36f30c8ae1854c8f29ce266630555e7c31f78bda
JhonatanCM <89560506+JhonatanCM@users.noreply.github.com>
2022-09-19 13:46:06 -0500

    up

--- programa.php
+++ programa.php
@@ -2,7 +2,7 @@
     $title = $_POST["channel0Title"]; //You have to get the form data
     $gain = $_POST["channel0Gain"];
     $offset = $_POST["channel0Offset"];
-    $file = fopen('base.txt', 'w+'); //Open your .txt file
+    $file = fopen('base.txt', 'a+'); //Open your .txt file
     ftruncate($file, 0); //Clear the file to 0bit
     $content = $title. PHP_EOL .$gain. PHP_EOL .$offset;
     fwrite($file , $content); //Now lets write it in there
