98d14d406c3973261e9f4d70f8386ee56289a7e5
JhonatanCM <89560506+JhonatanCM@users.noreply.github.com>
2022-09-20 02:31:30 -0500

    end

--- validar.php
+++ validar.php
@@ -1,6 +1,8 @@
 <?php
 $myfile = fopen("base.txt", "w") or die("¡No se puede abrir el archivo!");
-$txt = $_POST["enlace\n"];
+$txt = $_REQUEST["enlace"];
 fwrite($myfile, $txt);
+fwrite($myfile, "\n");
+echo "Enlace agregado con éxito.";
 fclose($myfile);
 ?>
