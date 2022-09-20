0fbae1e224eca9c917fb666d7ffb3b81a542fb3e
JhonatanCM <89560506+JhonatanCM@users.noreply.github.com>
2022-09-20 02:48:29 -0500

    deleted

--- validar.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myfile = fopen("base.txt", "w") or die("¡No se puede abrir el archivo!");
-$txt = $_REQUEST["enlace"];
-fwrite($myfile, $txt);
-fwrite($myfile, "\n");
-echo "Enlace agregado con éxito.";
-fclose($myfile);
-?>
