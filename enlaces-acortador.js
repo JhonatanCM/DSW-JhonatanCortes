ebd8b05f27de27da5aa0b31920645927cfe1e985
JhonatanCM <89560506+JhonatanCM@users.noreply.github.com>
2022-09-21 13:29:38 -0500

    Update enlaces-acortador.js

--- enlaces-acortador.js
+++ enlaces-acortador.js
@@ -11,8 +11,6 @@ if(tiene_parametro){
   document.getElementById("divcont").style.display = "block";
 }
 // -----------------------------------------------------------------
-<script src="http://unionj.buenhost.ml/enlaces.js"></script>
-const links = registro.concat(id);
 
 // Contador --------------------------------------------------------
 window.onload = actualizarReloj;
@@ -22,7 +20,7 @@ var contador = 10;
 function actualizarReloj() {
   document.getElementById('countdown').innerHTML = contador;
   if(contador==0){
-    document.getElementById("btn_acort").href = links;
+    document.getElementById("btn_acort").href = links[id];
   }else{
     contador-=1;
     setTimeout("actualizarReloj()",1000);
@@ -32,11 +30,11 @@ function actualizarReloj() {
 
 
 // Enlaces ---------------------------------------------------------  
-/* const links = [
+const links = [
 "https://www.google.com/1",   // #0 //
 "https://www.google.com/2",   // #1 //
 "https://www.google.com/3"    // #2 //
-];*/
+];
 // -----------------------------------------------------------------
 
 // BlockAdBlock ----------------------------------------------------
